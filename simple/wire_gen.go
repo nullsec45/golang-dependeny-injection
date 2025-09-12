369cc720eb4b1545a58371b6519995c3222a22ab
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-09-12 17:46:38 +0700

    feat : Generated results

--- simple/wire_gen.go
+++ simple/wire_gen.go
@@ -6,6 +6,10 @@
 
 package simple
 
+import (
+	"github.com/google/wire"
+)
+
 // Injectors from injector.go:
 
 func InitalizedService(isError bool) (*SimpleService, error) {
@@ -16,3 +20,25 @@ func InitalizedService(isError bool) (*SimpleService, error) {
 	}
 	return simpleService, nil
 }
+
+func InitalizedDatabaseRepository() *DatabaseRepository {
+	databasePostgreSQL := NewDatabasePostgreSQL()
+	databaseMongoDB := NewDatabaseMongoDB()
+	databaseRepository := NewDatabaseRepository(databasePostgreSQL, databaseMongoDB)
+	return databaseRepository
+}
+
+func InitializedFooBarService() *FooBarService {
+	fooRepository := NewFooRepository()
+	fooService := NewFooService(fooRepository)
+	barRepository := NewBarRepository()
+	barService := NewBarService(barRepository)
+	fooBarService := NewFooBarService(fooService, barService)
+	return fooBarService
+}
+
+// injector.go:
+
+var fooSet = wire.NewSet(NewFooRepository, NewFooService)
+
+var barSet = wire.NewSet(NewBarRepository, NewBarService)
